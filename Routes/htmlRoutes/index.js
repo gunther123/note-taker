e3595f2c49c6e68446703505cb1c38112138d76d
Steven Slocum <stevesl23@hotmail.com>
2021-11-07 11:22:13 -0600

    added catch all route for endpoints that don't exist and fixed bug where routes were not pulling correctly.

--- Routes/htmlRoutes/index.js
+++ Routes/htmlRoutes/index.js
@@ -1,6 +1,16 @@
 const path = require('path');
 const router = require('express').Router();
 
+//Route for Index.html file
 router.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '../../public/index.html'));
   });
+
+//Route for the /notes.html page
+
+//Route to catch all routes that do not exist for the site
+router.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../../public/index.html'));
+  });
+
+module.exports = router;
--- server.js
+++ server.js
@@ -3,9 +3,16 @@ const express = require('express');
 const PORT = process.env.PORT || 3001;
 const app = express();
 
+const htmlRoutes = require('./routes/htmlRoutes')
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('public'));
+
+//Add Api Routes
+
+//Add HTML Routes
+app.use('/', htmlRoutes);
 
 app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}!`);
