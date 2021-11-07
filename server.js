8748fe55586c871aad5e1c82d129b50822deb5b2
Steven Slocum <stevesl23@hotmail.com>
2021-11-07 15:38:29 -0600

    fixed build error due to capitalization issue with Routes folder

--- server.js
+++ server.js
@@ -3,8 +3,8 @@ const express = require('express');
 const PORT = process.env.PORT || 3001;
 const app = express();
 
-const apiRoutes = require('./routes/apiRoutes');
-const htmlRoutes = require('./routes/htmlRoutes');
+const apiRoutes = require('./Routes/apiRoutes');
+const htmlRoutes = require('./Routes/htmlRoutes');
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
